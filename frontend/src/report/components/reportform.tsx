952510f3bec214973c3d30bdf087080ed919b75f
Alex Nguyen <112521541+anguyen464@users.noreply.github.com>
2026-05-01 13:16:42 +1000

    Extension UI report (#53)
    * updated tlscertificatetransformed data structure to include description. modified sorting to include ascending and descending order. modified table to hide ID and include description.
    
    * updated README.md file to include Node.js requirement, and steps to install, build the extension and install the extension onto a chromium browser
    
    * updated readme.md to reflect changes to policy data structure. returned the comment within the guide in local testing section
    
    * updated clear all text to reset filters
    
    * updated tableFilters to include filtering by search function. changed filters icon

--- frontend/src/report/components/reportform.tsx
+++ frontend/src/report/components/reportform.tsx
@@ -1,16 +1,32 @@
-
 export default function GenerateReport() {
+
+    const noop = () => {};
 
     return(
         <div style={containerStyle}>
-            <p>The report content generation things will go here</p>
-            <p>This should include:</p>
-            <p> (1) Report Log containing all previously visited domains </p>
-            <p> (2) Colour-coded visual summary i.e. Red for errors, Yellow for warnings, Green for correct </p>
-            <p> (3) Filters for sorting and searching as well as report generation </p>
-        </div>
-    )
+            <div style={buttonContainer}>
+                <div 
+                    style={buttonStyle}
+                    onClick={noop}
+                    title="Open settings page"
+                    onMouseOver={(e) => (e.currentTarget.style.background = "#e2e6ea")}
+                    onMouseOut={(e) => (e.currentTarget.style.background = "#f8f9fa")}
+                    onKeyDown={(e) => e.key === 'Enter' && noop}
+                >
+                    GENERATE REPORT (currently does nothing)
+                </div>
+            </div>
 
+            <div>
+                <p>The report content generation things will go here</p>
+                <p>This should include:</p>
+                <p> (1) Report Log containing all previously visited domains </p>
+                <p> (2) Colour-coded visual summary i.e. Red for errors, Yellow for warnings, Green for correct </p>
+                <p> (3) Filters for sorting and searching as well as report generation </p>
+            </div>
+        </div>
+        
+    )
 }
 
 
@@ -20,4 +36,27 @@ const containerStyle: React.CSSProperties = {
     padding: "10px",
     overflowX: "auto", // Enable horizontal scroll
     maxHeight: "200px",
+};
+
+const buttonContainer: React.CSSProperties = {
+    display: "flex", // flexbox to align buttons
+    gap: "4px",
+    alignItems: "center",
+    padding: 6
+};
+
+
+const buttonStyle: React.CSSProperties = {
+    display: "flex",
+    alignItems: "center",
+    gap: "6px",
+    padding: 6,
+    background: "#ffffff",
+    border: "1px solid #d1d9e0",
+    borderRadius: "6px",
+    cursor: "pointer",
+    fontSize: "14px",
+    fontWeight: "500",
+    color: "#24292f",
+    transition: "background 0.2s"
 };
--- frontend/src/report/utils/tableUtils.ts
+++ frontend/src/report/utils/tableUtils.ts
@@ -1,29 +1,61 @@
 import type { TLSCertificateTransformed } from "../../sharedComponent/types";
 
 
-export function filterCertificates( data: TLSCertificateTransformed[], statusFilters:string[]) {
-    if (statusFilters.length === 3) {
-        return data;
-    }
-
-    return data.filter(cert => statusFilters.includes(cert.status));
+type Filters = {
+    status: string[];
+    protocol: string[];
 }
+
+
+
+export function filterCertificates( data: TLSCertificateTransformed[], filters:Filters) {
+    return data.filter(cert => {
+
+        const statusFilters = filters.status.length === 0 || filters.status.includes(cert.status);
+
+        const protocolFilters = filters.protocol.length === 0 || filters.protocol.includes(cert.protocol);
+
+        return statusFilters && protocolFilters;
+    });
+};
 
 
 export function sortCertificates( data: TLSCertificateTransformed[], sortBy: string) {
     const sorted = [...data];
 
     switch (sortBy) {
-        case "expiry":
+        case "expiryAsc":
+            return sorted.sort((a, b) => a.daysRemaining - b.daysRemaining);
+
+        case "domainAsc":
+            return sorted.sort((a, b) => a.subjectName.localeCompare(b.subjectName));
+
+        case "issuerAsc":
+            return sorted.sort((a, b) => a.issuer.localeCompare(b.issuer));
+
+        case "expiryDesc":
             return sorted.sort((a, b) => b.daysRemaining - a.daysRemaining);
         
-        case "domain":
+        case "domainDesc":
             return sorted.sort((a, b) => b.subjectName.localeCompare(a.subjectName));
 
-        case "issuer":
+        case "issuerDesc":
             return sorted.sort((a, b) => b.issuer.localeCompare(a.issuer));
 
         default:
             return sorted.sort((a, b) => Number(b.id) - Number(a.id));
     }
-}
+};
+
+
+export function searchCertificates(data: TLSCertificateTransformed[], query: string) {
+
+    if (!query.trim()) return data; // checks if query is empty. if so, return the entire data
+
+    const lowerQuery = query.toLowerCase(); // convert query to lower case
+
+    return data.filter(cert => 
+        cert.subjectName.toLowerCase().includes(lowerQuery) || // checks if part of the domain includes the query
+        cert.issuer.toLowerCase().includes(lowerQuery) // checks if part of the issuer includes the query
+    );
+};
